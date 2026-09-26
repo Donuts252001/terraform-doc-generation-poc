aade089a923cf7e64e72d67c1c7378bb065076ef
Vaish <95070666+Donuts252001@users.noreply.github.com>
2026-09-26 19:30:26 +0530

    Update s3.tf

--- terraform-iaac/s3.tf
+++ terraform-iaac/s3.tf
@@ -33,3 +33,21 @@ resource "aws_s3_bucket_versioning" "uploads_bucket_2" {
     status = "Disabled"
   }
 }
+
+resource "aws_s3_bucket" "uploads_bucket_3" {
+  bucket_prefix = "${var.environment}-uploads-bucket-3"
+
+  tags = {
+    Name        = "${var.environment}-uploads-3"
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "uploads_bucket_3" {
+  bucket = aws_s3_bucket.uploads_bucket_3.id
+
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
