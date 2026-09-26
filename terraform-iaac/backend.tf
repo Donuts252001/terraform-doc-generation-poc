f4bc199a1f0ac8583bdad30be8b168f387ac28bd
Vaish <95070666+Donuts252001@users.noreply.github.com>
2026-09-26 19:12:12 +0530

    Update backend.tf

--- terraform-iaac/backend.tf
+++ terraform-iaac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket"
+    bucket = "terraform-doc-generation-poc-state-bucket"
     key    = "terraform/dev/terraform.tfstate"
     region =  "eu-west-1"
   }
